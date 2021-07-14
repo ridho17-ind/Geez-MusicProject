9a98724867a986862c65efe5bb04f71a38dad36d
SkyzoGans <81844427+ridho17-ind@users.noreply.github.com>
2021-07-14 20:07:18 +0700

    Create private.py

---
+++ GeezProject/modules/private.py
@@ -0,0 +1,188 @@
+# Daisyxmusic (Telegram bot project )
+# Copyright (C) 2021  Inukaasith
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import logging
+from GeezProject.modules.msg import Messages as tr
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from GeezProject.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,BOT_USERNAME, OWNER
+logging.basicConfig(level=logging.INFO)
+
+@Client.on_message(
+    filters.command("start")
+    & filters.private
+    & ~ filters.edited
+)
+async def start_(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>Haii {message.from_user.first_name} saya adalah {PROJECT_NAME}\n
+Saya Adalah Bot Music Group, Yang dapat Memutar Lagu di Voice Chat Group Anda Dengan Mudah
+Saya Memiliki Banyak Fitur Seperti :
+
+➠ Memutar Musik.
+➠ Mendownload Lagu.
+➠ Mencari Lagu.
+➠ Mendownload Video Lagu.
+➠ Tekan » /help « Untuk Fitur Lainnya.
+
+
+🦁 Special Thanks To : {OWNER}
+🦊 Thank To Contributors
+- @XFLSkyzo
+- @YaguraGans
+- @SadboyInfo
+- @ZyansGans
+
+Ingin Menambahkan Saya ke Grup Anda? Tambahkan Saya Ke Group Anda!
+</b>""",
+
+# Edit Yang Seharusnya Lu Edit Aja:D
+# Tapi Jangan di Hapus Special Thanks To nya Yaaa :'D
+
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Tambahkan saya ke Grup Anda", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+                [
+                    InlineKeyboardButton(
+                        "Channel Updates", url=f"https://t.me/{UPDATES_CHANNEL}"), 
+                    InlineKeyboardButton(
+                        "Group Support", url=f"https://t.me/{SUPPORT_GROUP}")
+                ],[
+                    InlineKeyboardButton(
+                        "Contributor", url=f"https://t.me/sadroomsinfo/83")
+                ],[
+                    InlineKeyboardButton(
+                        "Donate", url=f"https://t.me/XFLSkyzo")
+                ]
+            ]
+        ),
+        reply_to_message_id=message.message_id
+        )
+
+@Client.on_message(filters.private & filters.incoming & filters.command(['help']))
+def _help(client, message):
+    client.send_message(chat_id = message.chat.id,
+        text = tr.HELP_MSG[1],
+        parse_mode="markdown",
+        disable_web_page_preview=True,
+        disable_notification=True,
+        reply_markup = InlineKeyboardMarkup(map(1)),
+        reply_to_message_id = message.message_id
+    )
+
+help_callback_filter = filters.create(lambda _, __, query: query.data.startswith('help+'))
+
+@Client.on_callback_query(help_callback_filter)
+def help_answer(client, callback_query):
+    chat_id = callback_query.from_user.id
+    disable_web_page_preview=True
+    message_id = callback_query.message.message_id
+    msg = int(callback_query.data.split('+')[1])
+    client.edit_message_text(chat_id=chat_id,    message_id=message_id,
+        text=tr.HELP_MSG[msg],    reply_markup=InlineKeyboardMarkup(map(msg))
+    )
+
+
+def map(pos):
+    if(pos==1):
+        button = [
+            [InlineKeyboardButton(text = 'Next »', callback_data = "help+2")]
+        ]
+    elif(pos==len(tr.HELP_MSG)-1):
+        url = f"https://t.me/{SUPPORT_GROUP}"
+        button = [
+            [InlineKeyboardButton("Tambahkan Saya Ke Grup Anda", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+            [InlineKeyboardButton(text = 'Channel Updates', url=f"https://t.me/{UPDATES_CHANNEL}"),
+             InlineKeyboardButton(text = 'Group Support', url=f"https://t.me/{SUPPORT_GROUP}")],
+            [InlineKeyboardButton(text = 'Contributor', url=f"https://t.me/sadroomsinfo/83")],
+            [InlineKeyboardButton(text = '«', callback_data = f"help+{pos-1}")]
+        ]
+    else:
+        button = [
+            [
+                InlineKeyboardButton(text = '«', callback_data = f"help+{pos-1}"),
+                InlineKeyboardButton(text = '»', callback_data = f"help+{pos+1}")
+            ],
+        ]
+    return button
+
+
+@Client.on_message(
+    filters.command("start")
+    & filters.group
+    & ~ filters.edited
+)
+async def start(client: Client, message: Message):
+    await message.reply_text(
+        "💁🏻‍♂️ **Apakah Anda ingin mencari Link YouTube?**",
+        reply_markup=InlineKeyboardMarkup(
+            [   
+                [    
+                    InlineKeyboardButton(
+                        "Iya", switch_inline_query_current_chat=""
+                    ),
+                    InlineKeyboardButton(
+                        "Gak", callback_data="close"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+@Client.on_message(
+    filters.command("help")
+    & filters.group
+    & ~ filters.edited
+)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        """**Klik Tombol dibawah untuk Melihat Cara Menggunakan Bot**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "📜 Cara Menggunakan BOT 📜", url="https://t.me/sadroomsinfo/52"
+                    )
+                ]
+            ]
+        ),
+    )  
+
+
+@Client.on_message(
+    filters.command("reload")
+    & filters.group
+    & ~ filters.edited
+)
+async def reload(client: Client, message: Message):
+    await message.reply_text("""Bot **berhasil dimulai ulang!**\n\n• **Daftar admin** telah **diperbarui**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Group Support", url=f"https://t.me/SadRoomsChat"
+                    ),
+                    InlineKeyboardButton(
+                        "Created By", url=f"https://t.me/sadroomsinfo/83"
+                    )
+                ]
+            ]
+        )
+   )
+
